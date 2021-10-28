27fc598dc214f275f4aeaa12cda8d9e33f13f9f9
John Grey <johngrey4296@gmail.com>
2021-10-28 14:58:43 +0100

    [gui] add initial gui code

--- sub_processor.py
+++ sub_processor.py
@@ -16,6 +16,10 @@ from os.path import split, isdir, splitext, expanduser
 from os import listdir
 import re
 import datetime
+import tkinter as tk
+import tkinter.ttk as ttk
+import argparse
+
 # Setup root_logger:
 from os.path import splitext, split
 import logging as root_logger
@@ -32,64 +36,74 @@ logging = root_logger.getLogger(__name__)
 TIME_PAIR = re.compile("^.+?-->")
 COUNTER   = re.compile("^[0-9]+$")
 
-# https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior
-# datetime.datetime.strptime(str, format)
+MAIN_SIZE = 300
 
-import argparse
-
-#see https://docs.python.org/3/howto/argparse.html
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
                                  epilog = "\n".join(["SRT Processor"]))
 parser.add_argument('-t', "--target", action="append")
 
-def process(text: str) -> List[str]:
-    words = text.split(" ")
-    result = []
-    summed = ""
-    for word in words[0:]:
-        if len(summed) < 42:
-            summed += " "
-            summed += word
-        else:
-            result.append(summed)
-            summed = word[:]
+class Application(tk.Frame):
 
-    result.append(summed)
-    return result
+    def __init__(self, master=None):
+        super().__init__(master)
+        master.minsize(width=MAIN_SIZE,height=MAIN_SIZE)
+        args = parser.parse_args()
+        if args.target is None:
+            logging.warning("No Targets Specified")
+            exit(1)
+        self.files = [abspath(expanduser(x)) for x in args.target]
 
-def main():
-    args = parser.parse_args()
-    if args.target is None:
-        logging.warning("No Targets Specified")
-        exit(1)
-    args.target = [abspath(expanduser(x)) for x in args.target]
-
-    lines : List[str] = []
-    for target in args.target:
-        with open(target, 'r') as f:
-            lines = f.readlines()
-
-        with open("{}.backup".format(target), 'w') as bkup:
-            bkup.write("\n".join(lines))
-
-        # Loop over lines, reformatting as necessary
-        cleaned : List[str] = []
-        joined : str = ""
-        for line in lines:
-            if not bool(line.strip()):
-                cleaned += process(joined)
-                cleaned.append(line)
-                joined = ""
-            elif COUNTER.match(line):
-                cleaned.append(line)
-            elif TIME_PAIR.match(line):
-                cleaned.append(line)
+    def process(self, text: str) -> List[str]:
+        words = text.split(" ")
+        result = []
+        summed = ""
+        for word in words[0:]:
+            if len(summed) < 42:
+                summed += " "
+                summed += word
             else:
-                joined += line
-                continue
+                result.append(summed)
+                summed = word[:]
 
-        with open(target, 'w') as f:
-            f.write("\n".join(cleaned))
+        result.append(summed)
+        return result
+
+    def process_files(self):
+        lines : List[str] = []
+
+        for target in self.files:
+            with open(target, 'r') as f:
+                lines = f.readlines()
+
+            with open("{}.backup".format(target), 'w') as bkup:
+                bkup.write("\n".join(lines))
+
+            # Loop over lines, reformatting as necessary
+            cleaned : List[str] = []
+            joined : str = ""
+            for line in lines:
+                if not bool(line.strip()):
+                    cleaned += process(joined)
+                    cleaned.append(line)
+                    joined = ""
+                elif COUNTER.match(line):
+                    cleaned.append(line)
+                elif TIME_PAIR.match(line):
+                    cleaned.append(line)
+                else:
+                    joined += line
+                    continue
+
+            with open(target, 'w') as f:
+                f.write("\n".join(cleaned))
+
+
+    def destroy(self):
+        """ Customised destroy method  """
+        logging.info("Closing program")
+        tk.Frame.destroy(self)
 
 if __name__ == '__main__':
-    main()
+    root = tk.Tk()
+    app = Application(master=root)
+    app.mainloop()
